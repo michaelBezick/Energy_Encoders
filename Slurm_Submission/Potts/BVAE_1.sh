7911dc932929a9b109cd111bed212952f0c8254b
michaelBezick <mbezick@purdue.edu>
2024-03-07 09:55:39 -0500

    Edits

--- Slurm_Submission/Potts/BVAE_1.sh
+++ Slurm_Submission/Potts/BVAE_1.sh
@@ -6,4 +6,4 @@ export WORLD_SIZE=NUM_GPUS
 export NODE_RANK=RANK
 export MASTER_ADDR=MASTER_NODE_IP
 export MASTER_PORT=PORT
-python -m torch.distributed.launch --nproc_per_node=2 ../../Models/Potts/BVAE_1.py
+python -m torch.distributed.launch --nproc_per_node=2 BVAE_1.py
