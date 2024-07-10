6c866d346d2ee7f291ad72ce60ff6c5951f01d10
michaelBezick <mbezick@purdue.edu>
2024-07-10 16:39:08 -0400

    Edits

---
+++ Evaluate_Model_Matching/convert_to_KID_format.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+
+def to_rgb(dataset):
+    return dataset.repeat(1, 3, 1, 1)
+
+def convert_to_KID_and_save(dataset, filename):
+    dataset = dataset[:, :, 0:32, 0:32]
+    dataset = to_rgb(dataset).numpy().astype(np.float16)
+    np.save(filename, dataset)
+
+
+
+# loaded_dataset_2nd = to_rgb(loaded_dataset_2nd).numpy().astype(np.float16)
+# loaded_dataset_3rd = to_rgb(loaded_dataset_3rd).numpy().astype(np.float16)
+# loaded_dataset_4th = to_rgb(loaded_dataset_4th).numpy().astype(np.float16)
+#
+# np.save("dataset_2nd.npy", loaded_dataset_2nd)
+# np.save("dataset_3rd.npy", loaded_dataset_3rd)
+# np.save("dataset_4th.npy", loaded_dataset_4th)
