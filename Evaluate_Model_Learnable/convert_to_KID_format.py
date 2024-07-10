ffebd0947a202670215c586ef6794b4bee17fb45
michaelBezick <mbezick@purdue.edu>
2024-07-10 16:22:38 -0400

    Edits

--- Evaluate_Model_Learnable/convert_to_KID_format.py
+++ Evaluate_Model_Learnable/convert_to_KID_format.py
@@ -2,6 +2,9 @@ from compress_dataset import load_dataset_from_binary
 import numpy as np
 import torch
 
+def to_rgb(dataset):
+    return dataset.repeat(1, 3, 1, 1)
+
 loaded_dataset_2nd = load_dataset_from_binary(
     "./compressed_2nd.bin", [20_000, 1, 32, 32]
 )
@@ -12,9 +15,10 @@ loaded_dataset_4th = load_dataset_from_binary(
     "./compressed_4th.bin", [20_000, 1, 32, 32]
 )
 
-loaded_dataset_2nd = loaded_dataset_2nd.numpy().astype(np.float16)
-loaded_dataset_3rd = loaded_dataset_3rd.numpy().astype(np.float16)
-loaded_dataset_4th = loaded_dataset_4th.numpy().astype(np.float16)
+
+loaded_dataset_2nd = to_rgb(loaded_dataset_2nd).numpy().astype(np.float16)
+loaded_dataset_3rd = to_rgb(loaded_dataset_3rd).numpy().astype(np.float16)
+loaded_dataset_4th = to_rgb(loaded_dataset_4th).numpy().astype(np.float16)
 
 np.save("dataset_2nd.npy", loaded_dataset_2nd)
 np.save("dataset_3rd.npy", loaded_dataset_3rd)
